e6f113c7ec5e9c28253d868df4d55d820ad707b3
f18m <francesco.montorsi@gmail.com>
2024-04-17 10:48:11 +0200

    f

--- raspy2mqtt/config.py
+++ raspy2mqtt/config.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import yaml
+from constants import *
 
 #
 # Author: fmontorsi
--- raspy2mqtt/raspy2mqtt.py
+++ raspy2mqtt/raspy2mqtt.py
@@ -21,25 +21,11 @@ import queue
 from datetime import datetime, timezone
 from stats import *
 from config import *
+from constants import *
 
 # =======================================================================================================
 # GLOBALs
 # =======================================================================================================
-
-THIS_SCRIPT_PYPI_PACKAGE = "ha-alarm-raspy2mqtt"
-MQTT_TOPIC_PREFIX = "home"
-MQTT_QOS_AT_LEAST_ONCE = 1
-
-# SequentMicrosystem-specific constants
-SEQMICRO_INPUTHAT_STACK_LEVEL = 0  # 0 means the first "stacked" board (this code supports only 1!)
-SEQMICRO_INPUTHAT_MAX_CHANNELS = 16
-SEQMICRO_INPUTHAT_SHUTDOWN_BUTTON_GPIO = 26  # GPIO pin connected to the push button
-SEQMICRO_INPUTHAT_INTERRUPT_GPIO = (
-    11  # GPIO pin connected to the interrupt line of the I/O expander (need pullup resistor)
-)
-SEQMICRO_INPUTHAT_I2C_SDA = 2  # reserved for I2C communication between Raspberry CPU and the input HAT
-SEQMICRO_INPUTHAT_I2C_SCL = 3  # reserved for I2C communication between Raspberry CPU and the input HAT
-
 
 # global dictionary of gpiozero.LED instances used to drive outputs
 g_output_channels = {}
