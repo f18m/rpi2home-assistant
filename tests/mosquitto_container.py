ee36095a58e5119b4e6a728ca57d9333e9b08e01
f18m <francesco.montorsi@gmail.com>
2026-03-06 23:59:04 +0100

    Fix tests providing writable /data

--- tests/mosquitto_container.py
+++ tests/mosquitto_container.py
@@ -33,6 +33,11 @@ class MosquittoContainerEnhanced(MosquittoContainer):
         self.watched_topics = {}
 
     def start(self) -> Self:
+        # since version 2.1.1 - 2026-02-04, which fixed a PUID/PGID issue, the container needs to write to the data directory, 
+        # so we need to map it to a volume
+        # see also upstream fix at https://github.com/testcontainers/testcontainers-python/pull/978
+        super().with_volume_mapping("mosquitto_data", "/data", mode="rw")
+
         # do container start
         super().start()
         # now add callback
